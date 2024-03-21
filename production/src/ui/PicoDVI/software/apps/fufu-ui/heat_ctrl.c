2474149c860758a501c788ad498182d8e6b02334
leomc1118 <classyleo1800@gmail.com>
2024-03-21 19:11:24 -0400

    added heat control convert temp func, understanding more

--- production/src/ui/PicoDVI/software/apps/fufu-ui/heat_ctrl.c
+++ production/src/ui/PicoDVI/software/apps/fufu-ui/heat_ctrl.c
@@ -3,16 +3,19 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
+#include "hardware/pwm.h"
 #include "pico/binary_info.h"
 
 #define i2c_bus 0
 #define IR_addr 0x36
-#define PWM_PIN 20
+#define PWM_PIN 31
+#define LED_PIN 29
 #define D_CYCLE 0.5
 
 void check_temp(uint8_t upper_byte){
 
-    // Check flags and raise alerts accordingly 
+    // Check temp limits and raise alerts accordingly 
+    // temperature limits subject to change
     if ((upper_byte & 0x40) == 0x40) { //TACTUAL > TUPPER
         heat_off();
         printf("Temperature is above the upper temperature limit.\n");
@@ -27,8 +30,22 @@ void check_temp(uint8_t upper_byte){
     }
 }
 
-void heat_on(){
+float convert_temp(uint8_t upper_byte, uint8_t lower_byte) {
 
+    float temperature;
+
+    if ((upper_byte & 0x10) == 0x10) {
+        upper_byte = upper_byte & 0x0F;
+        temperature = 256 - (((float) upper_byte * 16) + ((float) lower_byte / 16));
+    } else {
+        temperature = (((float) upper_byte * 16) + ((float) lower_byte / 16));
+    }
+
+    return temperature;
+}
+
+void heat_on(){
+    
 }
 
 void heaf_off(){
@@ -37,8 +54,17 @@ void heaf_off(){
 
 
 int main() {
-    pwm.freq(1000)
     gpio_init(PWM_PIN);
-    gpio_set_dir(PWM_PIN, GPIO_OUT);
-}
+    //pio_set_dir(PWM_PIN, GPIO_OUT);
+    gpio_set_function(PWM_PIN, GPIO_FUNC_PIN)
+    // figure out how to set level
+    //level = 
 
+    // if one wanted to obtain PWM generator slice and channel values, it's as shown:
+    //static uint   pwm_gpio_to_slice_num(uint gpio)
+    //static uint   pwm_gpio_to_channel(uint gpio)
+    //slice = pwm_gpio_to_slice_num (PWM_PIN); 
+    //channel = pwm_gpio_to_channel (PWM_PIN);
+
+    pwm_set_gpio_level(uint PWM_PIN, uint16_t level)
+}
