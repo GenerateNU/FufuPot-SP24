8c19407ed00b140dbdc415c13838f2e382e5ad23
leomc1118 <classyleo1800@gmail.com>
2024-03-20 22:07:55 -0400

    adding more thinking and skeleton code too tired

--- production/src/ui/PicoDVI/software/apps/fufu-ui/heat_ctrl.c
+++ production/src/ui/PicoDVI/software/apps/fufu-ui/heat_ctrl.c
@@ -8,14 +8,36 @@
 #define i2c_bus 0
 #define IR_addr 0x36
 #define PWM_PIN 20
-#define FREQ 20000
 #define D_CYCLE 0.5
 
-int read_temp(uint8_t IR_addr, ){
-    
+void check_temp(uint8_t upper_byte){
+
+    // Check flags and raise alerts accordingly 
+    if ((upper_byte & 0x40) == 0x40) { //TACTUAL > TUPPER
+        heat_off();
+        printf("Temperature is above the upper temperature limit.\n");
+    }
+    if ((upper_byte & 0x20) == 0x20) { //TACTUAL < TLOWER
+        heat_on();
+        printf("Temperature is below the lower temperature limit.\n");
+    }
+    if ((upper_byte & 0x80) == 0x80) { //TACTUAL > TCRIT
+        printf("Temperature is above the critical temperature limit.\n");    
+        exit();
+    }
 }
 
+void heat_on(){
+
+}
+
+void heaf_off(){
+
+}
+
+
 int main() {
+    pwm.freq(1000)
     gpio_init(PWM_PIN);
     gpio_set_dir(PWM_PIN, GPIO_OUT);
 }
