ab06dd91ca922ff58695b13617ff16bc3ae1daea
nscaperdas <scaperdas.n@northeastern.edu>
2024-04-05 12:20:47 -0400

    fuck leos code i had to comment it all out to build

--- production/src/ui/PicoDVI/software/apps/fufu-ui/main.cpp
+++ production/src/ui/PicoDVI/software/apps/fufu-ui/main.cpp
@@ -9,29 +9,34 @@
 #include "hardware/vreg.h"
 #include "pico/sem.h"
 
-#include "dvi.h"
-#include "dvi_serialiser.h"
-#include "common_dvi_pin_configs.h"
-#include "sprite.h"
 
+//#include "dvi.h"
+//#include "dvi_serialiser.h"
+//#include "common_dvi_pin_configs.h"
+//#include "sprite.h"
+
+
+/*
 #include "raspberry_128x128_rgab5515.h"
 #include "eben_128x128_rgab5515.h"
-
+*/
 #define FRAME_WIDTH 400
 #define FRAME_HEIGHT 240
 #define VREG_VSEL VREG_VOLTAGE_1_20
 #define DVI_TIMING dvi_timing_800x480p_60hz
 
 
-struct dvi_inst dvi0;
+//struct dvi_inst dvi0;
 
 void core1_main() {
+	/*
 	dvi_register_irqs_this_core(&dvi0, DMA_IRQ_0);
 	while (queue_is_empty(&dvi0.q_colour_valid))
 		__wfe();
 	dvi_start(&dvi0);
 	dvi_scanbuf_main_16bpp(&dvi0);
 	__builtin_unreachable();
+	*/
 }
 
 
